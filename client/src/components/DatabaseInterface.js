b8ad699e44688ac027bec8e22331c5bed2f6f22e
Sporego <contact@filbert.io>
2021-05-18 19:39:17 -0400

    touch DatabaseInterface in ./components

---
+++ client/src/components/DatabaseInterface.js
@@ -0,0 +1,16 @@
+import React, { useContext } from 'react';
+import {} from '@chakra-ui/react';
+import { ThemeContext } from '../contexts/ThemeContext';
+
+import {} from 'react-icons/fa';
+
+function DatabaseInterface() {
+  const { lightMode, setLightMode } = useContext(ThemeContext);
+
+  return (
+    <>
+    </>
+  );
+}
+
+export default DatabaseInterface;
