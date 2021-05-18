70b78029627267a05e852cd56d02ef173283b4af
Sporego <contact@filbert.io>
2021-05-18 19:23:47 -0400

    added github repo link

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,13 +1,14 @@
 import React, { useContext } from 'react';
-import { Box, Button, Flex, Heading } from '@chakra-ui/react';
+import { Box, Button, Flex, Heading, LinkBox, Link } from '@chakra-ui/react';
 import { ThemeContext } from '../contexts/ThemeContext';
 
-import { FaMoon, FaSun } from 'react-icons/fa';
+import { FaMoon, FaSun, FaGithub } from 'react-icons/fa';
 
 function Navbar() {
   const { lightMode, setLightMode } = useContext(ThemeContext);
 
   return (
+    <>
     <Box
       w="100%"
       py="4"
@@ -34,6 +35,29 @@ function Navbar() {
         </Button>
       </Flex>
     </Box>
+    <Box
+      w="100%"
+      pb="2"
+      color={lightMode === 'light' ? 'black' : 'white'}
+      bg={lightMode === 'light' ? 'white' : 'gray.800'}
+    >
+      <Flex direction="row" align="center" justify="space-around">
+        <Button
+        as={Link}
+        href="https://github.com/Sporego/hello-ryan"
+          rightIcon={<FaGithub/>}
+          variant="outline"
+          _hover={
+            lightMode === 'light'
+              ? { background: 'gray.200' }
+              : { background: 'gray.600' }
+          }
+        >
+          Github Repo
+        </Button>
+      </Flex>
+    </Box>
+    </>
   );
 }
 
