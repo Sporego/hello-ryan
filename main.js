ce09691cb55cd54ee5bed9fc3fd6a2aa66e1181b
Sporego <contact@filbert.io>
2021-05-18 22:35:59 -0400

    added dotenv for security reasons

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 import express from "express";
 import path from "path";
+import mongoose from "mongoose";
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
@@ -15,6 +16,6 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-app.listen(PORT, () => {
-  console.log(`Server listening on port: ${PORT}!`);
-});
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server listening on port: ${PORT}!`)))
+  .catch((error) => console.log(`${error} did not connect`));
