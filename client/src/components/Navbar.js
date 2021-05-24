60f8989b120a3be9d4f0a0b9b760abf62e08b220
Sporego <contact@filbert.io>
2021-05-23 23:32:08 -0400

    npx prettier --write Navbar.js

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -15,26 +15,31 @@ function Navbar() {
         color={lightMode === 'light' ? 'black' : 'white'}
         bg={lightMode === 'light' ? 'white' : 'gray.800'}
       >
-      <Center>
-        <Flex direction="row" align="center" justify="space-between" w={{base:"95%",md: "80%"}}>
-          <Heading as="h1" size="2xl">
-            Hello Ryan
-          </Heading>
-          <Button
-            rightIcon={lightMode === 'light' ? <FaSun /> : <FaMoon />}
-            variant="outline"
-            onClick={() => {
-              setLightMode(lightMode === 'light' ? 'dark' : 'light');
-            }}
-            _hover={
-              lightMode === 'light'
-                ? { background: 'gray.200' }
-                : { background: 'gray.600' }
-            }
+        <Center>
+          <Flex
+            direction="row"
+            align="center"
+            justify="space-between"
+            w={{ base: '95%', md: '80%' }}
           >
-            {lightMode === 'light' ? 'Light' : 'Dark'}
-          </Button>
-        </Flex>
+            <Heading as="h1" size="2xl">
+              Hello Ryan
+            </Heading>
+            <Button
+              rightIcon={lightMode === 'light' ? <FaSun /> : <FaMoon />}
+              variant="outline"
+              onClick={() => {
+                setLightMode(lightMode === 'light' ? 'dark' : 'light');
+              }}
+              _hover={
+                lightMode === 'light'
+                  ? { background: 'gray.200' }
+                  : { background: 'gray.600' }
+              }
+            >
+              {lightMode === 'light' ? 'Light' : 'Dark'}
+            </Button>
+          </Flex>
         </Center>
       </Box>
       <Box
@@ -43,23 +48,28 @@ function Navbar() {
         color={lightMode === 'light' ? 'black' : 'white'}
         bg={lightMode === 'light' ? 'white' : 'gray.800'}
       >
-      <Center>
-        <Flex direction="row" align="center" justify="space-between" w={{base:"95%",md: "80%"}}>
-          <Button
-            border="0"
-            as={Link}
-            href="https://github.com/Sporego/hello-ryan"
-            rightIcon={<FaGithub />}
-            variant="outline"
-            _hover={
-              lightMode === 'light'
-                ? { background: 'gray.200' }
-                : { background: 'gray.600' }
-            }
+        <Center>
+          <Flex
+            direction="row"
+            align="center"
+            justify="space-between"
+            w={{ base: '95%', md: '80%' }}
           >
-            Github Repo
-          </Button>
-        </Flex>
+            <Button
+              border="0"
+              as={Link}
+              href="https://github.com/Sporego/hello-ryan"
+              rightIcon={<FaGithub />}
+              variant="outline"
+              _hover={
+                lightMode === 'light'
+                  ? { background: 'gray.200' }
+                  : { background: 'gray.600' }
+              }
+            >
+              Github Repo
+            </Button>
+          </Flex>
         </Center>
       </Box>
     </>
