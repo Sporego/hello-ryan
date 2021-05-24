1b8c2ff1cdfba4f1d8f336397a4fe3a3099496be
Sporego <contact@filbert.io>
2021-05-24 09:42:19 -0400

    applicants route initial

--- routes/applicants.js
+++ routes/applicants.js
@@ -1,3 +1,10 @@
 import express from 'express';
 
+import {postApplicant,getApplicants} from '../controllers/applicants'
+
 const router = express.Router();
+
+router.get('/applicants/', getApplicants)
+router.post('/applicants/', postApplicant)
+
+export default router;
