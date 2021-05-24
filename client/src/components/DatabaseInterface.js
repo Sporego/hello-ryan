1cc16ee856b3bc85831dbd8680e44d41bb6f0f5b
Sporego <contact@filbert.io>
2021-05-24 13:15:46 -0400

    applicant POST completed

--- client/src/components/DatabaseInterface.js
+++ client/src/components/DatabaseInterface.js
@@ -15,7 +15,7 @@ import {
 import { ThemeContext } from '../contexts/ThemeContext';
 import { Formik, Form, Field, FieldArray, ErrorMessage } from 'formik';
 
-import {} from 'react-icons/fa';
+import axios from 'axios';
 
 function DatabaseInterface() {
   const { lightMode, setLightMode } = useContext(ThemeContext);
@@ -55,67 +55,96 @@ function DatabaseInterface() {
               <Heading>Applicant Tracking Interface</Heading>
               <Formik
                 initialValues={{}}
-                onSubmit={(values, actions) => {
-                  setTimeout(() => {
+                onSubmit={ async (values, actions) => {
+
+                    values.skills = values.skills.split(",")
                     alert(JSON.stringify(values, null, 2));
-                    actions.setSubmitting(false);
-                  }, 1000);
+                    try {
+                      const response = await axios.post('api/applicants', values)
+                      alert('Success! Applicant ID: '+response._id)
+                      actions.setSubmitting(false);
+                    } catch (error) {
+                      actions.setSubmitting(false);
+                      alert(error);
+                    }
+
+
+
                 }}
               >
-                {props => (
+                {({ values }) => (
                   <Form>
                     <HStack>
-                    <Field name="firstName" validate={validateFirstName}>
-                      {({ field, form }) => (
-                        <FormControl
-                          isInvalid={
-                            form.errors.firstName && form.touched.firstName
-                          }
-                        >
-                          <FormLabel htmlFor="firstName">First name</FormLabel>
-                          <Input
-                            {...field}
-                            id="firstName"
-                            placeholder="First name"
-                          />
-                          <FormErrorMessage>
-                            {form.errors.firstName}
-                          </FormErrorMessage>
-                        </FormControl>
-                      )}
-                    </Field>
-                    <Field name="lastName" validate={validateLastName}>
-                      {({ field, form }) => (
-                        <FormControl
-                          isInvalid={
-                            form.errors.lastName && form.touched.lastName
-                          }
-                        >
-                          <FormLabel htmlFor="lastName">Last name</FormLabel>
-                          <Input
-                            {...field}
-                            id="lastName"
-                            placeholder="Last name"
-                          />
-                          <FormErrorMessage>
-                            {form.errors.lastName}
-                          </FormErrorMessage>
-                        </FormControl>
-                      )}
-                    </Field>
-                    <FieldArray></FieldArray>
+                      <Field name="firstName" validate={validateFirstName}>
+                        {({ field, form }) => (
+                          <FormControl
+                            isInvalid={
+                              form.errors.firstName && form.touched.firstName
+                            }
+                          >
+                            <FormLabel htmlFor="firstName">
+                              First name
+                            </FormLabel>
+                            <Input
+                              {...field}
+                              id="firstName"
+                              placeholder="First name"
+                            />
+                            <FormErrorMessage>
+                              {form.errors.firstName}
+                            </FormErrorMessage>
+                          </FormControl>
+                        )}
+                      </Field>
+                      <Field name="lastName" validate={validateLastName}>
+                        {({ field, form }) => (
+                          <FormControl
+                            isInvalid={
+                              form.errors.lastName && form.touched.lastName
+                            }
+                          >
+                            <FormLabel htmlFor="lastName">Last name</FormLabel>
+                            <Input
+                              {...field}
+                              id="lastName"
+                              placeholder="Last name"
+                            />
+                            <FormErrorMessage>
+                              {form.errors.lastName}
+                            </FormErrorMessage>
+                          </FormControl>
+                        )}
+                      </Field>
                     </HStack>
+                    <Field name="skills">
+                        {({ field, form }) => (
+                          <FormControl
+                            isInvalid={
+                              form.errors.skills && form.touched.skills
+                            }
+                          >
+                            <FormLabel htmlFor="skills">Skills</FormLabel>
+                            <Input
+                              {...field}
+                              id="skills"
+                              placeholder="git,react,node"
+                            />
+                            <FormErrorMessage>
+                              {form.errors.skills}
+                            </FormErrorMessage>
+                          </FormControl>
+                        )}
+                      </Field>
                     <Button
                       mt={4}
                       colorScheme="green"
-                      isLoading={props.isSubmitting}
+                      isLoading={values.isSubmitting}
                       type="submit"
                     >
                       Submit
                     </Button>
                   </Form>
                 )}
-
               </Formik>
             </VStack>
           </Flex>
