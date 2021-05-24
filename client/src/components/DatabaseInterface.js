62bbe3c4e540e5dba8ee1693ce1ae3a22cac1c04
Sporego <contact@filbert.io>
2021-05-24 12:09:56 -0400

    Fname Lname added

--- client/src/components/DatabaseInterface.js
+++ client/src/components/DatabaseInterface.js
@@ -1,11 +1,40 @@
 import React, { useContext } from 'react';
-import { Box, Center, Flex, Heading } from '@chakra-ui/react';
+import {
+  Box,
+  Button,
+  Center,
+  Flex,
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Heading,
+  HStack,
+  Input,
+  VStack,
+} from '@chakra-ui/react';
 import { ThemeContext } from '../contexts/ThemeContext';
+import { Formik, Form, Field, FieldArray, ErrorMessage } from 'formik';
 
 import {} from 'react-icons/fa';
 
 function DatabaseInterface() {
   const { lightMode, setLightMode } = useContext(ThemeContext);
+
+  function validateFirstName(value) {
+    let error;
+    if (!value) {
+      error = 'First name is required';
+    }
+    return error;
+  }
+
+  function validateLastName(value) {
+    let error;
+    if (!value) {
+      error = 'Last name is required';
+    }
+    return error;
+  }
 
   return (
     <>
@@ -22,7 +51,73 @@ function DatabaseInterface() {
             justify="space-around"
             w={{ base: '95%', md: '80%' }}
           >
-            <Heading>Applicant Tracking Interface</Heading>
+            <VStack>
+              <Heading>Applicant Tracking Interface</Heading>
+              <Formik
+                initialValues={{}}
+                onSubmit={(values, actions) => {
+                  setTimeout(() => {
+                    alert(JSON.stringify(values, null, 2));
+                    actions.setSubmitting(false);
+                  }, 1000);
+                }}
+              >
+                {props => (
+                  <Form>
+                    <HStack>
+                    <Field name="firstName" validate={validateFirstName}>
+                      {({ field, form }) => (
+                        <FormControl
+                          isInvalid={
+                            form.errors.firstName && form.touched.firstName
+                          }
+                        >
+                          <FormLabel htmlFor="firstName">First name</FormLabel>
+                          <Input
+                            {...field}
+                            id="firstName"
+                            placeholder="First name"
+                          />
+                          <FormErrorMessage>
+                            {form.errors.firstName}
+                          </FormErrorMessage>
+                        </FormControl>
+                      )}
+                    </Field>
+                    <Field name="lastName" validate={validateLastName}>
+                      {({ field, form }) => (
+                        <FormControl
+                          isInvalid={
+                            form.errors.lastName && form.touched.lastName
+                          }
+                        >
+                          <FormLabel htmlFor="lastName">Last name</FormLabel>
+                          <Input
+                            {...field}
+                            id="lastName"
+                            placeholder="Last name"
+                          />
+                          <FormErrorMessage>
+                            {form.errors.lastName}
+                          </FormErrorMessage>
+                        </FormControl>
+                      )}
+                    </Field>
+                    <FieldArray></FieldArray>
+                    </HStack>
+                    <Button
+                      mt={4}
+                      colorScheme="green"
+                      isLoading={props.isSubmitting}
+                      type="submit"
+                    >
+                      Submit
+                    </Button>
+                  </Form>
+                )}
+
+              </Formik>
+            </VStack>
           </Flex>
         </Center>
       </Box>
