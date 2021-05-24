c0aea13985ad643342ddb44f3ea368dd5465b0df
Sporego <contact@filbert.io>
2021-05-24 00:07:09 -0400

    DBInterface initial

--- client/src/components/DatabaseInterface.js
+++ client/src/components/DatabaseInterface.js
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import {} from '@chakra-ui/react';
+import { Box, Center, Flex, Heading } from '@chakra-ui/react';
 import { ThemeContext } from '../contexts/ThemeContext';
 
 import {} from 'react-icons/fa';
@@ -7,7 +7,27 @@ import {} from 'react-icons/fa';
 function DatabaseInterface() {
   const { lightMode, setLightMode } = useContext(ThemeContext);
 
-  return <></>;
+  return (
+    <>
+      <Box
+        w="100%"
+        py="4"
+        color={lightMode === 'light' ? 'black' : 'gray.100'}
+        bg={lightMode === 'light' ? 'white' : 'gray.700'}
+      >
+        <Center>
+          <Flex
+            direction="row"
+            align="center"
+            justify="space-around"
+            w={{ base: '95%', md: '80%' }}
+          >
+            <Heading>Applicant Tracking Interface</Heading>
+          </Flex>
+        </Center>
+      </Box>
+    </>
+  );
 }
 
 export default DatabaseInterface;
